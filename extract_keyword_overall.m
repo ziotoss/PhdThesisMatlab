8cbf9be1234d1deeaf1de74a889fdceaab665e44
ziotoss <ziotoss@gmail.com>
2015-03-03 14:48:25 +0900

    Added extract_keyword_overall.m
    Extract keywords from all the documents.

--- extract_keyword_overall.m
+++ extract_keyword_overall.m
@@ -1,4 +1,4 @@
-function extract_keyword_overall(scratch, partition_nums, doc_length_limit, mean_divider)
+function extract_keyword_overall(scratch, doc_length_limit, mean_divider)
 % function extract_keyword_per_song
 %
 %   Input:
@@ -8,9 +8,9 @@ function extract_keyword_overall(scratch, partition_nums, doc_length_limit, mean
 %       mean_divider - variable to divide minimum word limit.
 %                      (Default value is 1)
 
-    if nargin < 3
+    if nargin < 2
         doc_length_limit = 50;
-        mean_divider = 1;
+        mean_divider = 2;
     end
 
     hts_folder = '_hts';
@@ -73,43 +73,66 @@ function extract_keyword_overall(scratch, partition_nums, doc_length_limit, mean
     time_elapsed = toc(start_time);
     fprintf(1, ' Done. Time elapsed is %.2f seconds.\n', time_elapsed);
 
+    partition_num = length(test_songs);
+    save([scratch filesep 'overall_test_songs_ln_partition_' num2str(partition_num) '.mat'], 'test_songs');
+
+    % Create an overall dictionary
+    clear start_time time_elapsed;
+    start_time = tic;
+    fprintf(1, 'Creating an overall unique dictionary.');
+    dictionary = [];
+    overall_episodes = 0;
+    for i = 1:partition_num
+        dictionary = [dictionary;test_songs(i).dictionary];
+        overall_episodes = overall_episodes + length(test_songs(i).episodes);
+    end
+    dictionary = unique(dictionary);
+    time_elapsed = toc(start_time);
+    fprintf(1, ' Done. Time elapsed is %.2f seconds.\n', time_elapsed);
+    
     % Create an overall occurrence matrix
     clear start_time time_elapsed;
     start_time = tic;
+    occ_mat = zeros(partition_num, length(dictionary));
     fprintf(1, 'Creating the occurrence matrix using the unique dictionary.');
-    for partition_num = partition_nums
-        for i = 1:length(test_songs)
-            episodes_per_partition = floor(length(test_songs(i).episodes) / partition_num);
-            test_songs(i).occ_mat = zeros(partition_num, length(test_songs(i).dictionary));
-            rand_idx = randperm(length(test_songs(i).episodes));
-            for j = 1:partition_num %length(test_songs(i).episodes)
-                partition_rand_idx = rand_idx((j - 1) * episodes_per_partition + 1:j * episodes_per_partition);
-                for k = partition_rand_idx
-                    words = test_songs(i).episodes(k).content_hts;
-                    for m = 1:length(words)
-                        idx = find(strcmp(test_songs(i).dictionary, words{m}) == 1);
-                        test_songs(i).occ_mat(j, idx) = test_songs(i).occ_mat(j, idx) + 1;
-                    end
-                end
+    for i = 1:partition_num
+        for j = 1:length(test_songs(i).episodes)
+            words = test_songs(i).episodes(j).content_hts;
+            for k = 1:length(words)
+                idx = find(strcmp(dictionary, words{k}) == 1);
+                occ_mat(i, idx) = occ_mat(i, idx) + 1;
             end
-
-            % Remove words that occur less than average per word occurrence.
-            average_word_occ = floor(sum(test_songs(i).occ_mat, 1) / size(test_songs(i).occ_mat, 2));
-            average_word_occ = floor(average_word_occ / mean_divider);
-            remove_idx = sum(test_songs(i).occ_mat, 1) <= average_word_occ;
-            test_songs(i).dictionary(remove_idx) = [];
-            test_songs(i).occ_mat(:, remove_idx) = [];
-
-            % Calculate Shannon's entropy
-            occ_mat_sum = sum(test_songs(i).occ_mat, 1);
-            p_w = test_songs(i).occ_mat ./ repmat(occ_mat_sum, size(test_songs(i).occ_mat, 1), 1);
-            S_w_tmp = sum(p_w .* log2(p_w + eps), 1);
-            S_w = (-1 / log2(partition_num)) * S_w_tmp;
-            S_ran = 1 - ((partition_num - 1) ./ (2 * occ_mat_sum * log2(partition_num)));
-            test_songs(i).E_w = (1 - S_w) ./ (1 - S_ran);
         end
-        save([scratch filesep 'test_songs_ln_partition_' num2str(partition_num) '.mat'], 'test_songs');
     end
+
+    % Remove words that occur less than average per word occurrence.
+    total_words = sum(sum(occ_mat));
+    average_word_occ = floor(total_words / overall_episodes);
+    average_word_occ = floor(average_word_occ / mean_divider);
+    remove_idx = sum(occ_mat, 1) <= average_word_occ;
+    dictionary(remove_idx) = [];
+    occ_mat(:, remove_idx) = [];
+    time_elapsed = toc(start_time);
+    fprintf(1, ' Done. Time elapsed is %.2f seconds.\n', time_elapsed);
+    
+    % Calculate Shannon's entropy
+    clear start_time time_elapsed;
+    start_time = tic;
+    fprintf(1, 'Calculating Shannons Entropy.');
+    n_w = sum(occ_mat, 1);
+    Ni = sum(occ_mat, 2);
+    fi_w = occ_mat ./ repmat(Ni, 1, size(occ_mat, 2));
+    fi_w_sum = sum(fi_w, 1);
+    p_w = fi_w ./ repmat(fi_w_sum, size(occ_mat, 1), 1);
+    S_w_tmp = sum(p_w .* log2(p_w + eps), 1);
+    S_w = (-1 / log2(partition_num)) * S_w_tmp;
+    S_ran = 1 - ((partition_num - 1) ./ (2 * n_w * log2(partition_num)));
+    E_w = (1 - S_w) ./ (1 - S_ran);
+    
+    results.dictionary = dictionary;
+    results.occ_mat = occ_mat;
+    results.E_w = E_w;
+    save([scratch filesep 'overall_test_songs_ln_result.mat'], 'results');
     time_elapsed = toc(start_time);
     fprintf(1, ' Done. Time elapsed is %.2f seconds.\n', time_elapsed);
 end
