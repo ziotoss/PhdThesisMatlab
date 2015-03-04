c024877e5ae65143dddce7bfef57121f768ac630
ziotoss <ziotoss@gmail.com>
2015-03-04 23:07:46 +0900

    added figures.

---
+++ get_keyword_figures.m
@@ -0,0 +1,23 @@
+function get_keyword_figures(keyword_ftr_file, top_n)
+
+    load(['scratch' filesep keyword_ftr_file '.mat']);
+    song_ftr = result.features;
+    keywords = result.keywords;
+    
+    fig_data = struct;
+    
+    for i = 1:length(song_ftr)
+        ftr = song_ftr(i).song_ftr;
+        top_n_idx = zeros(1, top_n);
+        for j = 1:top_n
+            [~, idx] = max(ftr);
+            ftr(idx) = 0;
+            top_n_idx(j) = idx;
+        end
+        fig_data(i).title = [song_ftr(i).song_artist ' - ' song_ftr(i).song_title];
+        fig_data(i).ftr = song_ftr(i).song_ftr;
+        fig_data(i).top_n_keywords = keywords(top_n_idx);
+    end
+
+    save(['scratch' filesep 'keyword_figure.mat'], 'fig_data');
+end
